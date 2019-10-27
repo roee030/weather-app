ef11b1e78098fd8f6819d0dcb407dd2bca1b1197
roee030 <roee.angel93@gmail.com>
2019-10-27 16:16:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var request = require('request');
 const url = "https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/37.8267,-122.4233"
 
 request({url:url},(error, response) => {
-    console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data)
 })
