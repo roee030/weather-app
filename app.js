5a26a2dd0e4aa2271b788bbe92873616492a940b
roee030 <roee.angel93@gmail.com>
2019-10-27 16:05:49 +0200

    using request

---
+++ app.js
@@ -0,0 +1,7 @@
+var request = require('request');
+
+const url = "https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/37.8267,-122.4233"
+
+request({url:url},(error, response) => {
+    console.log(response)
+})
