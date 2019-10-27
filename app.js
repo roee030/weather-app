139430f5b9ca7145c01848aabc453e2678078f63
roee030 <roee.angel93@gmail.com>
2019-10-27 18:12:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var request = require('request');
 
+const url = "https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/37.8267,-122.4233"
+
+request({url:url , json:true},(error, response) => {
+    console.log(response.body)
+})
+
+
 const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazI5M2VleTUwMzQ4M29wMnV0cHk2dXFrIn0.DyIk-WcyXZF2LY_u37FR9w"
 
 request({url:url , json:true },(error, response) => {
