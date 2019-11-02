f85479bcd8cd821f30e71cc4bdbeb510735b61fe
roee030 <roee.angel93@gmail.com>
2019-11-02 14:55:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var request = require('request');
 // const url1 = "https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/37.8267,-122.4233"
 
 // request({url:url1 , json:true},(error, response) => {
+//      if(error) console.log("There is an Error")
+//      else if( response.bode.error) console.log("enable to reach the server")
 //     console.log(response.body)
 // })
 
