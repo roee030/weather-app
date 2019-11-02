af2845a030646c6acf5d6e96f958167597047480
roee030 <roee.angel93@gmail.com>
2019-11-02 18:15:49 +0200

    update

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ var degreeConvert = require('./fTOc')
 
 const forecast = (longitude, latitude,callback) => {
     const url = "https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/"+latitude+","+longitude
-    request({url:url,json:true},(error,response) => {
+    request({url,json:true},(error,{body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + degreeConvert(response.body.currently.temperature) + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + degreeConvert(body.currently.temperature) + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -2,17 +2,17 @@ var request = require('request');
 
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address)+".json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazI5M2VleTUwMzQ4M29wMnV0cHk2dXFrIn0.DyIk-WcyXZF2LY_u37FR9w"
-    request({url:url , json:true},(error , response)=>{
+    request({url , json:true},(error , {body})=>{
         if(error)
         {
             callback('enable to connect to location service',undefined)
         }
-        else if(response.body.features.length === 0 ) callback('Unable to find location try again...',undefined)
+        else if(body.features.length === 0 ) callback('Unable to find location try again...',undefined)
         else{
             callback(undefined,{
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
