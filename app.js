8b61fabe84bf88941e81e01b82f53f41b1d6db00
roee030 <roee.angel93@gmail.com>
2019-11-02 15:39:54 +0200

    Update app.js
    using callbacks

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var request = require('request');
 // })
 
 
-const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazI5M2VleTUwMzQ4M29wMnV0cHk2dXFrIn0.DyIk-WcyXZF2LY_u37FR9w"
+const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/Tel%20Aviv.json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazI5M2VleTUwMzQ4M29wMnV0cHk2dXFrIn0.DyIk-WcyXZF2LY_u37FR9w"
 
 request({url:url2 , json:true },(error, response) => {
     if(error) console.log('enable to connect to location service')
@@ -19,4 +19,28 @@ request({url:url2 , json:true },(error, response) => {
     const latitude = response.body.features[0].center[1]
     const longitude = response.body.features[0].center[0]
     console.log(latitude,longitude)
+})
+
+
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address)+".json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazI5M2VleTUwMzQ4M29wMnV0cHk2dXFrIn0.DyIk-WcyXZF2LY_u37FR9w"
+    request({url:url , json:true},(error , response)=>{
+        if(error)
+        {
+            callback('enable to connect to location service',undefined)
+        }
+        else if(response.body.features.length === 0 ) callback('Unable to find location try again...',undefined)
+        else{
+            callback(undefined,{
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode('Tel Aviv',(error,data)=>{
+    console.log("Error",error)
+    console.log("data",data)
 })
