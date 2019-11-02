ce4af507e68cb3409be30ba513bbd15c67cb5b75
roee030 <roee.angel93@gmail.com>
2019-11-02 17:59:50 +0200

    Update app.js
    destructuring object

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ if(!address)
     console.log("Please enter an address")
 }
 else{
-    geocode(address,(error,data)=>{
+    geocode(address,(error,{latitude,longitude,location})=>{
         if(error){
            return console.log("Error",error)
         }
     
-        forecast(data.longitude,data.latitude,(error,forecastData)=>{
+        forecast(longitude,latitude,(error,forecastData)=>{
             if(error)
             {
                 return console.log("Error",error)
             }
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
      })
