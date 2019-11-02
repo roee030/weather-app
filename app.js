1cb837125cc259a2dec61ca2116cdc5e0143d2e2
roee030 <roee.angel93@gmail.com>
2019-11-02 17:23:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ var geocode = require('./utils/geocode')
 var forecast = require('./utils/forecast')
 
 
-
-geocode('Tel aviv',(error,data)=>{
-    if(error){
-       return console.log("Error",error)
-    }
-
-    forecast(data.longitude,data.latitude,(error,forecastData)=>{
-        if(error)
-        {
-            return console.log("Error",error)
+const address = process.argv[2]
+if(!address)
+{
+    console.log("Please enter an address")
+}
+else{
+    geocode(address,(error,data)=>{
+        if(error){
+           return console.log("Error",error)
         }
-        console.log(data.location)
-        console.log(forecastData)
-    })
-
-
-
-
-})
+    
+        forecast(data.longitude,data.latitude,(error,forecastData)=>{
+            if(error)
+            {
+                return console.log("Error",error)
+            }
+            console.log(data.location)
+            console.log(forecastData)
+        })
+     })
+}
