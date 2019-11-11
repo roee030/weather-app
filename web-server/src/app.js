11537b87ca6c380556f98b31bbe1962e645c3236
roee030 <roee.angel93@gmail.com>
2019-11-11 13:53:07 +0200

    serve HTML pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,10 @@ var express = require('express')
 var app = express()
 
 var publicDirectoryPath = path.join(__dirname,'../public')
-console.log(publicDirectoryPath)
 app.use(express.static(publicDirectoryPath))
 
 
-app.get('/',(req,res)=>{
-    res.send("Home page")
-})
-app.get('/help',(req,res)=>{
-    res.send("Help page")
-})
-app.get('/about',(req,res)=>{
-    res.send("About page")
-})
+
 app.get('/weather',(req,res)=>{
     res.send("Weather page")
 })
