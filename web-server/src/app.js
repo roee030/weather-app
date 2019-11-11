df4b2a95a765e63b139b4a64f1e951399c1b3fe8
roee030 <roee.angel93@gmail.com>
2019-11-11 13:20:37 +0200

    build app server
    start to use get, send , listen functions

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,2 +1,11 @@
 var express = require('express')
 var app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Home page")
+})
+
+
+app.listen(3000,()=>{
+    console.log("The server is running on port 3000")
+})
