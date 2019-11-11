40b38324b9cfcc20352e0b37b0cccf9411bcd0ab
roee030 <roee.angel93@gmail.com>
2019-11-11 13:47:30 +0200

    serve static page
    using use function I take an HTML an display on the web app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,25 @@
+var path = require('path')
 var express = require('express')
 var app = express()
+
+var publicDirectoryPath = path.join(__dirname,'../public')
+console.log(publicDirectoryPath)
+app.use(express.static(publicDirectoryPath))
+
 
 app.get('/',(req,res)=>{
     res.send("Home page")
 })
+app.get('/help',(req,res)=>{
+    res.send("Help page")
+})
+app.get('/about',(req,res)=>{
+    res.send("About page")
+})
+app.get('/weather',(req,res)=>{
+    res.send("Weather page")
+})
+
 
 
 app.listen(3000,()=>{
