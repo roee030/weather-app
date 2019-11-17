43be9710223e40d17bf394106afcf9588cf7f9e2
roee030 <roee.angel93@gmail.com>
2019-11-17 14:53:36 +0200

    add templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,10 @@ var express = require('express')
 var app = express()
 
 var publicDirectoryPath = path.join(__dirname,'../public')
+var viewsPath = path.join(__dirname, '../templates')
 app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
