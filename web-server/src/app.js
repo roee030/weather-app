814af544c5ddcd9961635a8a5dec32a1b4361647
roee030 <roee.angel93@gmail.com>
2019-11-17 15:11:55 +0200

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,9 +4,12 @@ var app = express()
 var hbs = require('hbs')
 var publicDirectoryPath = path.join(__dirname,'../public')
 var viewsPath = path.join(__dirname, '../templates/views')
+var partialsPath = path.join(__dirname,'../templates/partials')
+
+
 app.set('view engine', 'hbs')
 app.set('views',viewsPath)
-
+hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
