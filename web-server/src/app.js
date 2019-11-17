e873b4a4e10f16f479e63bd21b00fcc9b37a3d37
roee030 <roee.angel93@gmail.com>
2019-11-17 14:17:22 +0200

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,16 +3,36 @@ var express = require('express')
 var app = express()
 
 var publicDirectoryPath = path.join(__dirname,'../public')
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-
-
-app.get('/weather',(req,res)=>{
-    res.send("Weather page")
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
 
-app.listen(3000,()=>{
-    console.log("The server is running on port 3000")
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
