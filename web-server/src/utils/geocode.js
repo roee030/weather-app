7e30e5e1308e6d0f43f17a388848d29260fdd55b
roee030 <roee.angel93@gmail.com>
2019-11-26 16:04:31 +0200

    Update geocode.js

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoicm9lZTAzMCIsImEiOiJjazNmeGJhd2UwOHptM2pwZ2Y5MXh6bGo2In0.1BV81ji8_slY82I6_1VCBA'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
