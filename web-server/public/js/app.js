5f3e2e52e6638258441a6d793ea1f06f5c32c3a0
roee030 <roee.angel93@gmail.com>
2019-11-26 11:57:55 +0200

    add <p> to show the result weather

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,82 +1,26 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+console.log('Client side javascript file is loaded!')
 
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
-const myQuestions = [
-    {
-      question: "Who is the strongest?",
-      answers: {
-        a: "Superman",
-        b: "The Terminator",
-        c: "Waluigi, obviously"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "What is the best site ever created?",
-      answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Where is Waldo really?",
-      answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
-      },
-      correctAnswer: "d"
-    }
-  ];
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-function buildQuiz(){
-// we'll need a place to store the HTML output
-const output = [];
+    const location = search.value
 
-// for each question...
-myQuestions.forEach(
-  (currentQuestion, questionNumber) => {
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
-    // we'll want to store the list of answer choices
-    const answers = [];
-
-    // and for each available answer...
-    for(letter in currentQuestion.answers){
-
-      // ...add an HTML radio button
-      answers.push(
-        `<label>
-          <input type="radio" name="question${questionNumber}" value="${letter}">
-          ${letter} :
-          ${currentQuestion.answers[letter]}
-        </label>`
-      );
-    }
-
-    // add this question and its answers to the output
-    output.push(
-      `<div class="question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join('')} </div>`
-    );
-  }
-);
-
-// finally combine our output list into one string of HTML and put it on the page
-quizContainer.innerHTML = output.join('');
-
-
-
-}
-
-function showResults(){}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
