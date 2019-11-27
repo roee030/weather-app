fb6e8ebedffa1bb09e5350735567e494141dc798
roee030 <roee.angel93@gmail.com>
2019-11-27 20:03:41 +0200

    update other function with the converter function

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,5 +1,5 @@
 const request = require('request')
-
+const converterDegree = require('../utils/convertDegree')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/30d4fcb9f68c9cbdd5118de3bb0acad4/' + latitude + ',' + longitude
 
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + converterDegree(body.currently.temperature) + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
